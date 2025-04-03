046a8d705d3ff9226b2e7b4c2cd0376604153f7f
Rusin Alexandr <i@arusin.ru>
2025-04-03 11:47:01 +0300

    feat: дорабатывает страницу 404

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,3 +1,37 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const timerValue = 5;
+
 export function NotFound() {
-  return <h1 className="page-name-title">Упс... Ничего не найдено</h1>;
+  const [expires, setExpires] = useState(timerValue);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      navigate("/");
+    }, timerValue * 1000);
+
+    const intervalId = setInterval(() => {
+      setExpires((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <>
+      <h1 className="page-name-title">
+        Упс... <br />
+        Ничего не найдено
+      </h1>
+      <br />
+      Вы будете автоматические перенаправлены на главную страницу через{" "}
+      {expires}...
+    </>
+  );
 }
